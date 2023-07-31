20962a9fead9ce10781da57d0d1361934473b0b5
eoniser <eone.neone@gmail.com>
2023-07-31 20:22:00 +0300

    made functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+function hiEnglish(name) {
+    return 'Hello :D' 
+}
+
+function hiRussian(name) {
+    return 'Привет :D' 
+}
+
+function hiFrench(name) {
+    return 'Bonjour :D' 
+}
+
+function hiSpanish(name) {
+    return 'Hola :D' 
+}
+
+function hiChinese(name) {
+    return 'Nǐ hǎo :D' 
+}
+
+function hiItalian(name) {
+    return 'Ciao :D' 
+}
+
+module.exports = {hiEnglish, hiRussian, hiFrench, hiSpanish, hiChinese, hiItalian}
+
